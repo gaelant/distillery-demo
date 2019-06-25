405ea5966817190d3a430bd6813b993cce895356
gaelant <gaelan.taylor@akelius.de>
2019-06-25 10:46:08 +0200

    Finish demo by adding a new version of clock app and live deploying with upgrade release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Clock.Mixfile do
   def project do
     [
       app: :clock,
-      version: "0.1.0",
+      version: "2.2.2",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
